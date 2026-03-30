bc3ea1d1688a9e91021a4999c4fc524c85f57a48
Chimney343 <mk.kominek@gmail.com>
2026-03-30 14:14:30 +0200

    chore: Add debug script and rate cache data

---
+++ __debug_gbp.py
@@ -0,0 +1,15 @@
+﻿import sqlite3, pandas as pd
+fpath = r"C:\Users\mkkom\Mój dysk\PersonalFinance\MMEX\personal_finance.mmb"
+conn = sqlite3.connect(fpath)
+rows = conn.execute("""
+  SELECT t.TRANSID, t.TRANSDATE, t.TRANSCODE, t.TRANSAMOUNT, t.TOTRANSAMOUNT,
+         cur.CURRENCY_SYMBOL, a.ACCOUNTNAME
+  FROM CHECKINGACCOUNT_V1 t
+  JOIN ACCOUNTLIST_V1 a ON t.ACCOUNTID = a.ACCOUNTID
+  JOIN CURRENCYFORMATS_V1 cur ON a.CURRENCYID = cur.CURRENCYID
+  WHERE cur.CURRENCY_SYMBOL = "GBP"
+  AND t.TRANSDATE BETWEEN "2024-02-22" AND "2024-02-25"
+""").fetchall()
+for r in rows:
+    print(r)
+conn.close()
